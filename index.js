8892435e045c7867996d7ca51a2692344d8e5b7c
korosoangela2-lang <korosoangela2@gmail.com>
2026-04-15 09:26:25 +0300

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const datejs = require('datejs');
+
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  args.forEach(arr => {
+    combinedObject.users.push(...arr);
+  });
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 
-
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+module.exports = {    
+  ...combineUsers(combineUsers !== "undefined" && {combineUsers }),  
 };
