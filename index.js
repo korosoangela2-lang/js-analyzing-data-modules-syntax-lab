c626dbe4085b92c421214030f406319002b366f5
korosoangela2-lang <korosoangela2@gmail.com>
2026-04-15 12:09:01 +0300

    index.js

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const datejs = require('datejs');
-
+require("datejs");
 function combineUsers(...args) {
   const combinedObject = {
-    users: []
+    users: [],
   };
-
-  args.forEach(arr => {
-    combinedObject.users.push(...arr);
-  });
-
-  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+  for (i = 0; i < args.length; i++) {
+    const userArray = args[i];
+    combinedObject.users.push(...userArray);
+  }
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
 
   return combinedObject;
 }
 
-
-module.exports = {    
-  ...combineUsers(combineUsers !== "undefined" && {combineUsers }),  
+module.exports = {
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
